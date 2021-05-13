a47655ad7b63fe7f7c70a8322d4885a22fa0972a
Benjamin Cheng <bencheng200@gmail.com>
2021-05-13 14:22:14 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,66 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
- const counter = document.getElementById('counter')
- const plus = document.getElementById('+')
- const minus = document.getElementById('-')
- const pause = document.getElementById('pause')
- const like = document.getElementById('<3')
- const likes = document.getElementsByClassName('likes')[0]
- const submit = document.getElementById('submit')
- const commentForm = document.getElementsByTagName('input')[0]
- const comments = document.getElementById('list')
- let speedCounter = setInterval(plusCounter, 1000)
- let gameRunning = true
+var i =-1;
+var j = 0;
+var interval = setInterval( increment, 1000);
+function increment(){
+    i++;
+    document.querySelector('h1#counter').textContent = i;
+    j = 0;
+}
+increment();
 
-   function plusCounter() {
-     if (gameRunning === true){
-      counter.innerHTML++
-    }
-   };
+document.getElementById("minus").addEventListener("click", function(){
+  i--;
+  document.querySelector('h1#counter').textContent = i;
+});
 
-   plus.addEventListener("click", function() {
-     counter.innerHTML++
-   });
+document.getElementById("plus").addEventListener("click", function(){
+  i++;
+  document.querySelector('h1#counter').textContent = i;
+});
 
-   minus.addEventListener("click", function() {
-     counter.innerHTML--
-   });
-
-   like.addEventListener("click", function() {
-     const likesList = document.createElement("LI");
-     let likeTracker = document.createTextNode(`${counter.innerHTML} has been liked 1 times.`)
-     likesList.appendChild(likeTracker);
-     likes.appendChild(likesList);
-   });
-
-   submit.addEventListener("click", function() {
-     event.preventDefault();
-     const newComment = document.createElement("p");
-     let commentInfo = document.createTextNode(`${commentForm.value}`);
-     newComment.appendChild(commentInfo);
-     comments.appendChild(newComment);
-     commentForm.value ='';
-   });
-
-
- pause.addEventListener("click", function() {
-  if (gameRunning === true){
-    gameRunning = false
-    pause.innerHTML = "resume";
-    like.disabled = true;
-    minus.disabled = true;
-    plus.disabled = true;
-    submit.disabled = true;
-    commentForm.disabled = true;
+document.getElementById("heart").addEventListener("click", function(){
+  j++;
+  var li = document.createElement("li");
+  var node = document.createTextNode(i + " has been liked " + j + " time(s).")
+  var likes = document.querySelector('.likes')
+  var lastLike = document.querySelector('.likes').lastChild
+  li.appendChild(node);
+  if(j>1){
+   lastLike.replaceWith(li)
+  } else {
+    likes.appendChild(li);
   }
-  else {
-    gameRunning = true;
-    pause.innerHTML = "pause"
-    like.disabled = false;
-    minus.disabled = false;
-    plus.disabled = false;
-    submit.disabled = false;
-    commentForm.disabled = false;
-  };
- });
+});
 
+document.getElementById("pause").addEventListener("click", function(){
+  if(document.querySelector('#pause').innerText == "pause") {
+    clearInterval(interval)
+    document.querySelector('#pause').innerText = "resume"
+    document.getElementById("minus").disabled = true;
+    document.getElementById("plus").disabled = true;
+    document.getElementById("heart").disabled = true;
+    document.getElementById("submit").disabled = true;
+  } else {
+    interval = setInterval( increment, 1000);
+    document.querySelector('#pause').innerText = "pause"
+    document.getElementById("minus").disabled = false;
+    document.getElementById("plus").disabled = false;
+    document.getElementById("heart").disabled = false;
+    document.getElementById("submit").disabled = false;
+  }
+});
+
+document.getElementById("submit").addEventListener("click", function(event){
+  event.preventDefault();
+  var comment = document.querySelector('input#comment-input').value
+  var commentsList = document.querySelector('.comments')
+  var p = document.createElement("p");
+  var node = document.createTextNode(comment)
+  p.appendChild(node);
+  commentsList.appendChild(p);
+  document.querySelector('input#comment-input').value = ''
 });
