8f215473168d499a8c216425dd4a3c589a6276d1
Benjamin Cheng <bencheng200@gmail.com>
2021-05-13 14:20:19 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,66 @@
-//"use strict";
-function _toConsumableArray(a){
-  if(Array.isArray(a)){
-    for(var b=0,c=Array(a.length);
-    b<a.length;
-    b++)c[b]=a[b];
-    return c}
-    return Array.from(a)}
-    var playing=!0,timer=function(){
-      return setInterval(function(){
-        var a=document.getElementById("counter"),b=parseInt(a.innerText);
-        a.innerText=b+1},1e3)},interval=timer(),minus=document.getElementById("minus"),plus=document.getElementById("plus"),heart=document.getElementById("heart"),pause=document.getElementById("pause"),commentForm=document.getElementsByTagName("form")[0];minus.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b-1}),plus.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b+1}),heart.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText),c=document.querySelector(".likes"),d=void 0;if([].concat(_toConsumableArray(c.children)).map(function(a){return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');var e=parseInt(d.children[0].innerText);d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)}),pause.addEventListener("click",function(){playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),[].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause"!==a.id&&(a.disabled=!playing)})}),commentForm.addEventListener("submit",function(a){a.preventDefault();var b=this.children[0],c=b.value;b.value="";var d=document.querySelector(".comments"),e=document.createElement("p");e.innerText=c,d.appendChild(e)});
+document.addEventListener("DOMContentLoaded", function() {
+ const counter = document.getElementById('counter')
+ const plus = document.getElementById('+')
+ const minus = document.getElementById('-')
+ const pause = document.getElementById('pause')
+ const like = document.getElementById('<3')
+ const likes = document.getElementsByClassName('likes')[0]
+ const submit = document.getElementById('submit')
+ const commentForm = document.getElementsByTagName('input')[0]
+ const comments = document.getElementById('list')
+ let speedCounter = setInterval(plusCounter, 1000)
+ let gameRunning = true
+
+   function plusCounter() {
+     if (gameRunning === true){
+      counter.innerHTML++
+    }
+   };
+
+   plus.addEventListener("click", function() {
+     counter.innerHTML++
+   });
+
+   minus.addEventListener("click", function() {
+     counter.innerHTML--
+   });
+
+   like.addEventListener("click", function() {
+     const likesList = document.createElement("LI");
+     let likeTracker = document.createTextNode(`${counter.innerHTML} has been liked 1 times.`)
+     likesList.appendChild(likeTracker);
+     likes.appendChild(likesList);
+   });
+
+   submit.addEventListener("click", function() {
+     event.preventDefault();
+     const newComment = document.createElement("p");
+     let commentInfo = document.createTextNode(`${commentForm.value}`);
+     newComment.appendChild(commentInfo);
+     comments.appendChild(newComment);
+     commentForm.value ='';
+   });
+
+
+ pause.addEventListener("click", function() {
+  if (gameRunning === true){
+    gameRunning = false
+    pause.innerHTML = "resume";
+    like.disabled = true;
+    minus.disabled = true;
+    plus.disabled = true;
+    submit.disabled = true;
+    commentForm.disabled = true;
+  }
+  else {
+    gameRunning = true;
+    pause.innerHTML = "pause"
+    like.disabled = false;
+    minus.disabled = false;
+    plus.disabled = false;
+    submit.disabled = false;
+    commentForm.disabled = false;
+  };
+ });
+
+});
